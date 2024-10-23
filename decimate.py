3a3bc3d55da61fdecccf8af9f95d18c495406513
Clément Herbin <herbincl@bde.enseeiht.fr>
2024-10-23 15:09:02 +0200

    update main, add attribut, update compress_model

--- decimate.py
+++ decimate.py
@@ -2,58 +2,48 @@
 
 import obja
 import numpy as np
-import sys
 import objToGraphe
 import utils
+import networkx as nx
 
 class Decimater(obja.Model):
     def __init__(self):
         super().__init__()
         self.deleted_faces = set()
+        self.graph = nx.empty_graph()
 
-    # def contract(self, output, graph):
-    #     """
-    #     Decimates the model stupidly, and write the resulting obja in output.
-    #     """
-    #     operations = []
+    def load_graph(self, filename):
+        self.vertices, self.faces = objToGraphe.load_obj(filename)
+        self.graph = objToGraphe.create_graph(self.vertices, self.faces)
 
-    #     # Iterate through the vertex
-    #     for (vertex_index, vertex) in enumerate(self.vertices):
+    def get_graph(self):
+        return self.graph
 
-    #         # Iterate through the faces
-    #         for (face_index, face) in enumerate(self.faces):
+    def compress_model(self, output):
+        compressed = False
+        operations = []
+        while not compressed:
+            step_operations = self.step_compression()
+            if len(step_operations) > 0:
+                operations = operations + step_operations
+            else:
+                compressed = True
 
-    #             # Delete any face related to this vertex
-    #             if face_index not in self.deleted_faces:
-    #                 if vertex_index in [face.a,face.b,face.c]:
-    #                     self.deleted_faces.add(face_index)
-    #                     # Add the instruction to operations stack
-    #                     operations.append(('face', face_index, face))
-
-    #         # Delete the vertex
-    #         operations.append(('vertex', vertex_index, vertex))
-
-    #     # To rebuild the model, run operations in reverse order
-    #     operations.reverse()
-
-    #     # Write the result in output file
-    #     output_model = obja.Output(output, random_color=True)
-
-    #     for (ty, index, value) in operations:
-    #         if ty == "vertex":
-    #             output_model.add_vertex(index, value)
-    #         elif ty == "face":
-    #             output_model.add_face(index, value)   
-    #         else:
-    #             output_model.edit_vertex(index, value)
-
-    def compress_model(self, graph):
-        # TODO
-        return
+        operations.reverse()
+        
+        output_model = obja.Output(output, random_color=True)
+        
+        for (ty, index, value) in operations:
+            if ty == "vertex":
+                output_model.add_vertex(index, value)
+            elif ty == "face":
+                output_model.add_face(index, value)
+            else:
+                output_model.edit_vertex(index, value)
 
     # check OK
-    def step_compression(self, graph, faces, output):
-        mst = objToGraphe.minimum_spanning_tree(graph)
+    def step_compression(self):
+        mst = objToGraphe.minimum_spanning_tree(self.graph)
         collapsed_vertices = set()
         new_edges = []
         edges = list(mst.edges())
@@ -66,14 +56,14 @@ class Decimater(obja.Model):
                 continue
 
             # Récupérer les voisins
-            neighbors_v1 = set(graph.neighbors(v1))
-            neighbors_v2 = set(graph.neighbors(v2))
+            neighbors_v1 = set(self.graph.neighbors(v1))
+            neighbors_v2 = set(self.graph.neighbors(v2))
             neighbors = neighbors_v1.union(neighbors_v2)
 
             valid = True
-            # 
+            
             for w in neighbors:
-                if w != v1 and w != v2 and not utils.is_valid_triangle(v1, v2, w, graph, faces):
+                if w != v1 and w != v2 and not utils.is_valid_triangle(v1, v2, w, self.graph, self.faces):
                     valid = False
                     break
 
@@ -86,7 +76,7 @@ class Decimater(obja.Model):
             if valid:
                 collapsed_vertices.add(v2)
                 
-                for face_index, face in enumerate(faces):
+                for face_index, face in enumerate(self.faces):
                     if v2 in [face.a, face.b, face.c]:
                         self.deleted_faces.add(face_index)
                         operations.append(('face', face_index, face))
@@ -99,28 +89,17 @@ class Decimater(obja.Model):
                         for neighbor2 in neighbors_v1:
                             if neighbor2 != neighbor and neighbor2 not in collapsed_vertices:
                                 new_face = utils.create_face(v1, neighbor, neighbor2) 
-                                faces.append(new_face)  
-                                face_index = len(faces) - 1  
+                                self.faces.append(new_face)  
+                                face_index = len(self.faces) - 1  
                                 
-                                operations.append(('edit_vertex', neighbor, graph.nodes[neighbor]))  
-                vertex_v2 = graph.nodes[v2]  
+                                operations.append(('edit_vertex', neighbor, self.graph.nodes[neighbor]))  
+                vertex_v2 = self.graph.nodes[v2]  
                 operations.append(('vertex', v2, vertex_v2))
 
-                graph.remove_node(v2)
+                self.graph.remove_node(v2)
 
-        graph.add_edges_from(new_edges)
+        self.graph.add_edges_from(new_edges)
 
-        operations.reverse()
-
-        output_model = obja.Output(output, random_color=True)
-        
-        for (ty, index, value) in operations:
-            if ty == "vertex":
-                output_model.add_vertex(index, value)
-            elif ty == "face":
-                output_model.add_face(index, value)
-            else:
-                output_model.edit_vertex(index, value)
 
 
 def main():
@@ -130,17 +109,14 @@ def main():
     np.seterr(invalid = 'raise')
     model = Decimater()
     filename='example/bunny.obj'
-    
-    #model.parse_file(filename)
-    vertices, faces = objToGraphe.load_obj(filename)
-    graph = objToGraphe.create_graph(vertices, faces)
-    
-    model.compress_model(graph, faces)
+
+    model.load_graph(filename)
+    graph = model.get_graph()
+
     objToGraphe.visualize_mst_simple(graph, objToGraphe.minimum_spanning_tree(graph))
     
     with open('example/suzanne.obja', 'w') as output:
-        model.contract(output)
-    objToGraphe.minimum_spanning_tree(graph)
+        model.compress_model(output)
 
 if __name__ == '__main__':
     main()
